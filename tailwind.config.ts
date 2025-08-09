75612d5b223f5e40e2fa6a6ef7a45b7e43e7403c
Ro parts <droproparts@gmail.com>
2025-08-09 13:12:19 +0000

    i think problem in font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
+        body: ['Inter', 'PT Sans', 'sans-serif'],
         headline: ['Playfair Display', 'serif'],
         code: ['monospace'],
       },
